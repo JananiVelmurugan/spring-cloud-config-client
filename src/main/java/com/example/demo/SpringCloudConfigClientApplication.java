6ac9045283895253c52510b963b7d13512df5a72
Janani <jananise@gmail.com>
2018-01-26 04:01:41 +0530

    modified pom

--- src/main/java/com/example/demo/SpringCloudConfigClientApplication.java
+++ src/main/java/com/example/demo/SpringCloudConfigClientApplication.java
@@ -3,24 +3,27 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@RefreshScope
 @SpringBootApplication
 @RestController
 public class SpringCloudConfigClientApplication {
 
 	@Value("${db_user}")
 	String dbUser;
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudConfigClientApplication.class, args);
 	}
-	
-	@RequestMapping(value="/db", method=RequestMethod.GET)
-	public String getUser(){
+
+	@RequestMapping(value = "/db", method = RequestMethod.GET)
+	public String getUser() {
 		return dbUser;
 	}
-	                                                                 
+
 }
